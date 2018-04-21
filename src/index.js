b3c6d0dc717126d0e0c1cd8e032ecb331ac95e13
VMashanov <vvmashanov@yandex.ru>
2018-04-21 15:35:24 +0300

    index.js was changed

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import { certificates, certificatesAsync } from './certificates';
+import { sign, signAsync } from './sign';
+import {
+  paramsForDetachedSignature,
+  paramsForDetachedSignatureAsync
+} from './params_for_detached_signature';
+import { digestValue, digestValueAsync } from './digest_value';
+
+require('../vendor/cadesplugin_api'); // Include with webpack
+
+// provide access to cadesplugin_api
+const { cadesplugin } = window;
+
+/**
+ * @class
+ * @name CryptoProProvider
+ * @description Module provide methods for signing requests with Crypto Pro
+ * @author Vitaly Mashanov <vvmashanov@yandex.ru>
+ */
+
+/**
+ * @function
+ * @name isAsync
+ * @description Checking, which method used by browser (Async or NPAPI)
+ * @return {boolean}
+ */
+const isAsync = () => (cadesplugin.CreateObjectAsync ? true : false);
+
+module.exports = {
+  certificates: isAsync() ? certificatesAsync : certificates,
+  sign: isAsync() ? signAsync : sign,
+  paramsForDetachedSignature: isAsync() ? paramsForDetachedSignatureAsync : paramsForDetachedSignature,
+  digestValue: isAsync() ? digestValueAsync : digestValue,
+};
