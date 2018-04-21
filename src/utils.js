5a394a3a2620785de3cb15f65a70e9faff7e88e3
VMashanov <vvmashanov@yandex.ru>
2018-04-21 15:12:26 +0300

    Supports functions were filled into utils.js

---
+++ src/utils.js
@@ -0,0 +1,49 @@
+/**
+ * @function
+ * @name convertDate
+ * @description Method returns date
+ * @return {string} date
+ */
+export const convertDate = () => {
+  const date = new Date();
+  const navigator_name = navigator.appName;
+  return navigator_name == 'Microsoft Internet Explorer' ? date.getVarDate() : date
+}
+
+/**
+ * @function
+ * @name convertStringToObj
+ * @description Method convert string into object
+ * @param {string} str - string for convert
+ * @return {object} converted string
+ */
+export const convertStringToObj = (str) => {
+  const obj = new Object();
+
+  str.split(', ').map(el => {
+    obj[el.split('=')[0]] = el.split('=')[1]
+  })
+
+  return obj;
+}
+
+/**
+ * @function
+ * @name _hexToBase64
+ * @description Method convert hex into base64
+ * @param {string} hex - string for convert
+ * @param {string} str - empty string
+ * @param {string} index - start position of substring
+ * @return {string} converted base64 string
+ */
+export const hexToBase64 = (hex, str, index) => {
+  if (index >= 0) {
+    return hexToBase64(
+      hex,
+      str + String.fromCharCode(parseInt(hex.substr(index, 2), 16)),
+      index - 2
+    )
+  }
+
+  return window.btoa(str);
+}
