ff24cd69e519df2340ac0074f89d3073671ec065
VMashanov <vvmashanov@yandex.ru>
2018-08-12 12:43:05 +0300

    [UPDATED] refactoring of certificates function

--- src/certificates.js
+++ src/certificates.js
@@ -1,5 +1,40 @@
-import { cadesplugin, CADESCOM_CURRENT_USER_STORE } from './constants';
+import {
+  CreateObjectAsync,
+  CADESCOM_CURRENT_USER_STORE,
+} from './constants';
 import { convertStringToObj } from './utils';
+
+/**
+ * @function
+ * @name certificates
+ * @description Provides access to loaded certificates for browser (Async)
+ * @return {array} list of certificates
+ */
+const certificates = async () => {
+  const store = await CreateObjectAsync('CAPICOM.Store');
+  await store.Open(CADESCOM_CURRENT_USER_STORE);
+  const availableCertificates = await store.Certificates;
+  const amountOfCertificates = await availableCertificates.Count;
+  const arrayOfCertificates = collectListOfCertificats(
+    availableCertificates,
+    amountOfCertificates,
+    1,
+  );
+
+  return arrayOfCertificates;
+};
+
+const collectListOfCertificats =
+  async (certificates, amountOfCertificates, i, result = []) =>
+    (i > amountOfCertificates
+      ? result
+      : collectListOfCertificats(
+        certificates,
+        amountOfCertificates,
+        i + 1,
+        result.concat(await objectifyCertificate(await certificates.Item(i))),
+      )
+    );
 
 const objectifyCertificate = async (certificate) => {
   const isValid = await certificate.IsValid();
@@ -17,36 +52,6 @@ const objectifyCertificate = async (certificate) => {
   };
 
   return objectWithCertificate;
-};
-
-const collectListOfCertificats =
-  async (certificates, amountOfCertificates, i, result = []) =>
-    (i > amountOfCertificates
-      ? result
-      : collectListOfCertificats(
-        certificates,
-        amountOfCertificates,
-        i + 1,
-        result.concat(await objectifyCertificate(await certificates.Item(i))),
-      )
-    );
-
-/**
- * @function
- * @name certificatesAsync
- * @description Provides access to loaded certificates for browser (Async)
- * @return {array} list of certificates
- */
-
-const certificates = async () => {
-  const store = await cadesplugin.CreateObjectAsync('CAPICOM.Store');
-  await store.Open(CADESCOM_CURRENT_USER_STORE);
-  const availableCertificates = await store.Certificates;
-  const amountOfCertificates = await availableCertificates.Count;
-  const arrayOfCertificates
-    = collectListOfCertificats(availableCertificates, amountOfCertificates, 1);
-
-  return arrayOfCertificates;
 };
 
 export default certificates;
